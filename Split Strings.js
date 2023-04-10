e8be05a2d30b006b0b37d8fb5aeac3a283b8cf0c
MaksimTitovets <126208757+MaksimTitovets@users.noreply.github.com>
2023-04-10 23:18:07 +0400

    Update Split Strings.js

--- Split Strings.js
+++ Split Strings.js
@@ -1,3 +1,13 @@
+/*
+DESCRIPTION:
+Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+
+Examples:
+
+* 'abc' =>  ['ab', 'c_']
+* 'abcdef' => ['ab', 'cd', 'ef']
+*/
+
 function solution(str){
   let arr = str.split("");
   let arr2 = [];

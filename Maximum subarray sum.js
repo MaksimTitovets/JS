6a98871c1d441e4a28921b73f6f94a6cec9970bf
MaksimTitovets <126208757+MaksimTitovets@users.noreply.github.com>
2023-04-13 03:12:07 +0400

    Update Maximum subarray sum.js

--- Maximum subarray sum.js
+++ Maximum subarray sum.js
@@ -21,3 +21,13 @@ var maxSequence = function(arr){
   }
   return sum;
 }
+
+var maxSequenceBestPractice = function(arr){
+  var min = 0, ans = 0, i, sum = 0;
+  for (i = 0; i < arr.length; ++i) {
+    sum += arr[i];
+    min = Math.min(sum, min);
+    ans = Math.max(ans, sum - min);
+  }
+  return ans;
+}

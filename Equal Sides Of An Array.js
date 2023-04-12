353076294803dd46710bb37c627b2f0f96d35b75
MaksimTitovets <126208757+MaksimTitovets@users.noreply.github.com>
2023-04-12 23:37:38 +0400

    Update Equal Sides Of An Array.js

--- Equal Sides Of An Array.js
+++ Equal Sides Of An Array.js
@@ -49,3 +49,16 @@ function findEvenIndex(arr) {
       });
   } else return 0;
 }
+
+function findEvenIndexBestPractice(arr)
+{
+  var left = 0, right = arr.reduce(function(pv, cv) { return pv + cv; }, 0);
+  for(var i = 0; i < arr.length; i++) {
+      if(i > 0) left += arr[i-1];
+      right -= arr[i];
+      
+      if(left == right) return i;
+  }
+  
+  return -1;
+}

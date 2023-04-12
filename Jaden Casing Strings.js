2220fe1556c1ab1d75e887bac8ae2a7bb34f3e11
MaksimTitovets <126208757+MaksimTitovets@users.noreply.github.com>
2023-04-13 03:09:37 +0400

    Update Jaden Casing Strings.js

--- Jaden Casing Strings.js
+++ Jaden Casing Strings.js
@@ -13,3 +13,11 @@ Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
 String.prototype.toJadenCase = function () {
   return this.replace(/^.| ./g, (m) => m.toUpperCase());
 };
+
+/*The best practice:
+String.prototype.toJadenCase = function () { 
+  return this.split(" ").map(function(word){
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  }).join(" ");
+}
+*/

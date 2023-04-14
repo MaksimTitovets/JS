b9cd83e31107a70e7718ac2310f62f615d294eaf
Maksim Titovets <mks.titovets@gmail.com>
2023-04-14 23:50:44 +0400

    Update So Many Permutations!.js

--- So Many Permutations!.js
+++ So Many Permutations!.js
@@ -42,3 +42,12 @@ function permutations(string) {
   }
   return arr;
 }
+
+function permutationsBestPractice(str) {
+ return (str.length <= 1) ? [str] :
+         Array.from(new Set(
+           str.split('')
+              .map((char, i) => permutations(str.substr(0, i) + str.substr(i + 1)).map(p => char + p))
+              .reduce((r, x) => r.concat(x), [])
+         ));
+}

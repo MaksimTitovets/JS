6ec9205caee3ab45bf77011429357f6d8f13843b
Maksim Titovets <mks.titovets@gmail.com>
2023-04-15 23:55:37 +0400

    Update Tribonacci Sequence.js

--- Tribonacci Sequence.js
+++ Tribonacci Sequence.js
@@ -25,3 +25,10 @@ function tribonacci(signature,n){
   signature.splice(n);
   return signature;
 }
+
+function tribonacciBestPractice(signature,n){  
+  for (var i = 0; i < n-3; i++) { // iterate n times
+    signature.push(signature[i] + signature[i+1] + signature[i+2]); // add last 3 array items and push to trib
+  }
+  return signature.slice(0, n); //return trib - length of n
+}

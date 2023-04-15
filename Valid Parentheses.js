1a7a4012cd08b1cff3d1db2a15921761be4a3ce3
Maksim Titovets <mks.titovets@gmail.com>
2023-04-15 23:56:55 +0400

    Update Valid Parentheses.js

--- Valid Parentheses.js
+++ Valid Parentheses.js
@@ -17,3 +17,14 @@ function validParentheses(parens) {
     parens = parens.replace(/\(\)/, '');
   return (parens == []);
 }
+
+function validParenthesesBestPractice(parens){
+  var n = 0;
+  for (var i = 0; i < parens.length; i++) {
+    if (parens[i] == '(') n++;
+    if (parens[i] == ')') n--;
+    if (n < 0) return false;
+  }
+  
+  return n == 0;
+}

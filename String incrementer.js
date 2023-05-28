93e68d2b1cd4cce520005d90fc2477728933e607
Maksim Titovets <mks.titovets@gmail.com>
2023-05-28 12:36:36 +0400

    Update String incrementer.js

--- String incrementer.js
+++ String incrementer.js
@@ -1,6 +1,28 @@
+/*
+Your job is to write a function which increments a string, to create a new string.
+
+If the string already ends with a number, the number should be incremented by 1.
+If the string does not end with a number. the number 1 should be appended to the new string.
+Examples:
+
+foo -> foo1
+
+foobar23 -> foobar24
+
+foo0042 -> foo0043
+
+foo9 -> foo10
+
+foo099 -> foo100
+
+Attention: If the number has leading zeros the amount of digits should be considered.
+*/
+
 function incrementString(strng) {
-  //console.log(strng.replace(/\d+$/, x => x.match(/0/g).slice(2).join('') + Number(x) + 1));
-  let reg = strng.reverse().match(/(0+)/);
-  console.log(reg);
- // return strng.replace(/\d+$/, x => x.match(/0/g).slice(2).join('') + Number(x) + 1);
+  let s = strng.replace(/\d+$/, ''),
+      n = strng.replace(s, ''),
+      r = String(Number(n) + 1);  
+  while (n.length > r.length)
+    r = '0' + r;
+  return s + r;
 }

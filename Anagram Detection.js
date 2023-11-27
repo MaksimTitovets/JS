5596530e69d1b158c7473b1ccd6b15a035505560
Maksim Titovets <mks.titovets@gmail.com>
2023-11-26 20:13:48 -0500

    Update Anagram Detection.js

--- Anagram Detection.js
+++ Anagram Detection.js
@@ -5,6 +5,7 @@ Complete the function to return true if the two arguments given are anagrams of 
 Examples
 "foefet" is an anagram of "toffee"
 "Buckethead" is an anagram of "DeathCubeK"
+
 */
 
 var isAnagram = function(test, original) {

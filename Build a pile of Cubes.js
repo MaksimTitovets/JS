144062886319910731a32fe3ca49dd11f6106393
Maksim Titovets <mks.titovets@gmail.com>
2023-12-10 23:48:35 -0500

    Update Build a pile of Cubes.js

--- Build a pile of Cubes.js
+++ Build a pile of Cubes.js
@@ -2,9 +2,11 @@
 You are given the total volume m of the building. Being given m can you find the number n of cubes you will have to build?
 
 Examples:
+
 findNb(1071225) --> 45
 
 findNb(91716553919377) --> -1
+
 */
 
 function findNb(m) {

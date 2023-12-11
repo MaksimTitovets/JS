5938e699fd7adf14688cf8b0c3618a6a8ba2f6c5
Maksim Titovets <mks.titovets@gmail.com>
2023-12-10 23:48:03 -0500

    Update Build Tower.js

--- Build Tower.js
+++ Build Tower.js
@@ -1,6 +1,7 @@
 /*
 Build Tower
-Build a pyramid-shaped tower, as an array/list of strings, given a positive integer number of floors. A tower block is represented with "*" character.
+Build a pyramid-shaped tower, as an array/list of strings, given a positive integer number of floors.
+A tower block is represented with "*" character.
 
 For example, a tower with 3 floors looks like this:
 
@@ -9,6 +10,7 @@ For example, a tower with 3 floors looks like this:
   " *** ", 
   "*****"
 ]
+
 And a tower with 6 floors looks like this:
 
 [
@@ -19,6 +21,7 @@ And a tower with 6 floors looks like this:
   " ********* ", 
   "***********"
 ]
+
 */
 
 function towerBuilder(nFloors) {

f153e873c6c37d87e3779b923e8124c7b47f8dbb
Maksim Titovets <mks.titovets@gmail.com>
2023-12-17 19:12:11 -0500

    Update Counting sheep.js

--- Counting sheep.js
+++ Counting sheep.js
@@ -1,9 +1,9 @@
 /*
+
 Consider an array/list of sheep where some sheep may be missing from their place. 
 We need a function that counts the number of sheep present in the array (true means present).
 
 For example,
-
 [true,  true,  true,  false,
   true,  true,  true,  true ,
   true,  false, true,  false,
@@ -12,7 +12,8 @@ For example,
   false, false, true,  true]
 The correct answer would be 17.
 
-Hint: Don't forget to check for bad values like null/undefined
+Hint: Don't forget to check for bad values like null/undefined.
+
 */
 
 function countSheeps(arrayOfSheep) {
